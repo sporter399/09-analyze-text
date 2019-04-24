814c6935c42e55a4725d4e21541d19950e13aa13
Steve Porter <sjporter399@gmail.com>
2019-04-24 10:37:57 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -2,3 +2,18 @@
 
 def analyze_text(text):
     # Your code here
+    alpha_count = 0
+    e_count = 0
+
+    for i in text:
+        if i.isalpha() == True:
+            alpha_count += 1
+            if i == 'e':
+                e_count += 1
+            if i == 'E':
+                e_count += 1
+
+    e_percent = (e_count / alpha_count) * 100
+
+    return ("The text contains " + repr(alpha_count) + " alphabetic characters, of which " + repr(e_count) + " "
+          + "(" + repr(e_percent) + "%)" + " are 'e'.")
